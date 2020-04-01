8801477c52bc474f53ecb2188eb08b9ad403cc5d
Ian Abuaf <abuafi@usi.ch>
2020-04-01 23:58:36 +0200

    Tutors and Meetings

--- Usilearn/app/src/main/java/com/example/usilearn/MainActivity.java
+++ Usilearn/app/src/main/java/com/example/usilearn/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static int algo = View.GONE;
     public static int ca = View.GONE;
     public static int calc = View.GONE;
+    public static boolean meeting=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.algob).setVisibility(algo);
         findViewById(R.id.calcb).setVisibility(calc);
         findViewById(R.id.cab).setVisibility(ca);
+        if (meeting) {
+            TextView view = findViewById(R.id.meetings);
+            view.setText(R.string.yesmeet);
+        } else {
+            TextView view = findViewById(R.id.meetings);
+            view.setText(R.string.nomeet);
+        }
     }
 
     public void openNewTopic(View view) {
@@ -36,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void openCa(View view){
         Intent intent = new Intent(this, CaMain.class);
+        startActivity(intent);
+    }
+
+    public void cancelMeeting(View view) {
+        MainActivity.meeting = false;
+        Toast toast = Toast.makeText(this, "You canceled the meeting.", Toast.LENGTH_SHORT);
+        toast.show();
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
--- Usilearn/app/src/main/java/com/example/usilearn/Tutors.java
+++ Usilearn/app/src/main/java/com/example/usilearn/Tutors.java
@@ -5,23 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-public class AlgoMain extends AppCompatActivity {
+public class Tutors extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_algo_main);
+        setContentView(R.layout.activity_tutors);
     }
 
-    public void remAlgo(View view) {
-        MainActivity.algo=View.GONE;
+    public void organizeMeeting(View view) {
+        MainActivity.meeting=true;
+        Toast toast = Toast.makeText(this, "You scheduled a meeting!", Toast.LENGTH_SHORT);
+        toast.show();
         Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void openTutors(View view) {
-        Intent intent = new Intent(this, Tutors.class);
         startActivity(intent);
     }
 }
